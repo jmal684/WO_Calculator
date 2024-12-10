d70faed1fc5d3f380242220f6157b239959e7bc7
JMal <jmalecki684@gmail.com>
2024-12-10 20:32:34 +0100

    Added updateBinaryRepresentationScreen function

--- src/main/java/com/example/wo_prototyp/MainController.java
+++ src/main/java/com/example/wo_prototyp/MainController.java
@@ -10,6 +10,8 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -25,7 +27,7 @@ public class MainController implements Initializable {
 
     @FXML
     private ToggleGroup number_systems, number_formats;
-    private List<ToggleButton> binaryRepresentationButtons;
+    private ArrayList<ToggleButton> binaryRepresentationButtons;
 
 
 
@@ -37,13 +39,13 @@ public class MainController implements Initializable {
         initializeNumberFormatsToggleGroup();
     }
     private void initializeBinaryRepresentationToggleButtons(){
-        binaryRepresentationButtons = List.of(new ToggleButton[]{btn_bit0, btn_bit1, btn_bit2, btn_bit3, btn_bit4, btn_bit5, btn_bit6, btn_bit7, btn_bit8, btn_bit9, btn_bit10,
+        binaryRepresentationButtons = new ArrayList<ToggleButton>(Arrays.asList(new ToggleButton[]{btn_bit0, btn_bit1, btn_bit2, btn_bit3, btn_bit4, btn_bit5, btn_bit6, btn_bit7, btn_bit8, btn_bit9, btn_bit10,
                 btn_bit11, btn_bit12, btn_bit13, btn_bit14, btn_bit15, btn_bit16, btn_bit17, btn_bit18, btn_bit19, btn_bit20,
                 btn_bit21, btn_bit22, btn_bit23, btn_bit24, btn_bit25, btn_bit26, btn_bit27, btn_bit28, btn_bit29, btn_bit30,
                 btn_bit31, btn_bit32, btn_bit33, btn_bit34, btn_bit35, btn_bit36, btn_bit37, btn_bit38, btn_bit39, btn_bit40,
                 btn_bit41, btn_bit42, btn_bit43, btn_bit44, btn_bit45, btn_bit46, btn_bit47, btn_bit48, btn_bit49, btn_bit50,
                 btn_bit51, btn_bit52, btn_bit53, btn_bit54, btn_bit55, btn_bit56, btn_bit57, btn_bit58, btn_bit59, btn_bit60, btn_bit61,
-                btn_bit62, btn_bit63});
+                btn_bit62, btn_bit63}));
 
         for(ToggleButton toggleButton: binaryRepresentationButtons)
         {
@@ -72,10 +74,30 @@ public class MainController implements Initializable {
                 RadioButton clicked = (RadioButton) t1.getToggleGroup().getSelectedToggle();
 
                 //TODO Implement converting to different number formats, preferably using a switch
+                updateBinaryRepresentationScreen(clicked.getText());
             }
         });
     }
 
+
+    // updating view components
+    private void updateBinaryRepresentationScreen(String numberFormat){
+        int avaibleBits =
+                switch (numberFormat)
+                {
+                    case "Qword" -> 64;
+                    case "Dword" -> 32;
+                    case "Word" -> 16;
+                    case "Byte" -> 8;
+                    default -> 0;
+                };
+
+        for (int i = binaryRepresentationButtons.size() - 1; i >= 0; i--)
+            binaryRepresentationButtons.get(i).setVisible(i < avaibleBits);
+
+
+        //TODO Disable bit markings
+    }
 
 
 }
