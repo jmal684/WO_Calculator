9b9ec9fa5fab0e48baa840a888d449a1788cdb58
JMal <jmalecki684@gmail.com>
2024-12-10 09:21:15 +0100

    Added ids to toggle buttons in binary representation screen

--- src/main/java/com/example/wo_prototyp/MainController.java
+++ src/main/java/com/example/wo_prototyp/MainController.java
@@ -3,7 +3,7 @@ package com.example.wo_prototyp;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
